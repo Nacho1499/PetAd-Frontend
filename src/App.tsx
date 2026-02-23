ff50685307d8ab2bbcb712e17f66d6fb8faa6cc8
Ezedike-Evan <egwomevan323@gmail.com>
2026-02-23 14:00:30 +0100

    feat: Add forget and reset password pages, along with reusable form input and submit button components, and update the authentication layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import RegisterPage from "./pages/RegisterPage";
+import ForgetPasswordPage from "./pages/forgetPasswordPage";
 
 function App() {
   return (
-    <RegisterPage />
+    <ForgetPasswordPage />
   );
 }
 
